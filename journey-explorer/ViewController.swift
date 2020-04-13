20d4d1dda2327aee117571aa148acc50f7fecd59
Tony Varian Yoditanto <tonyoditanto@gmail.com>
2020-04-13 09:27:04 +0700

    Initial Commit

---
+++ journey-explorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  journey-explorer
+//
+//  Created by Tony Varian Yoditanto on 13/04/20.
+//  Copyright © 2020 Tony Varian Yoditanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
